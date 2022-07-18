550f3638dd403141bb123c091ea0828c5400ac27
Daniel Carvalho <danielcarvalhodev@gmail.com>
2022-07-17 21:36:57 -0300

    Jenkins

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+echo "Jenkins";
+exit;
 
 if (strtolower(getenv('APP_ENV') == 'development')) {
     define('C3_CODECOVERAGE_ERROR_LOG_FILE', '/storage/logs/c3_error.log'); //Optional (if not set the default c3 output dir will be used)
