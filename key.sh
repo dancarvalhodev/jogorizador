a9dfda3a5a588c9cb8cd4d11c178c8116fe7196b
Daniel Carvalho <danielcarvalhodev@gmail.com>
2022-08-27 21:43:04 -0300

    :new: Jenkinsfile

--- key.sh
+++ key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /data/keys/oauth
+cd /usr/share/nginx/html/data/keys/oauth
 openssl genrsa -out private.key 2048
 openssl rsa -in private.key -pubout -out public.key
 cd ..
-chmod -R 777 oauth/
+chmod -R 777 /usr/share/nginx/html/data/keys/oauth
